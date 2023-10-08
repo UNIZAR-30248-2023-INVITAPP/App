7d5bbf7baf4dc71324ba8b805ea8c3d766683586
IvanBesZamorano <blwond@icloud.com>
2023-10-09 01:29:48 +0200

    Primera version Frontend

---
+++ app/src/main/java/com/taquillacentral/invitapp/LeerDNI.java
@@ -0,0 +1,14 @@
+package com.taquillacentral.invitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeerDNI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leer_dni);
+    }
+}
---
+++ app/src/main/java/com/taquillacentral/invitapp/SeleccionarEvento.java
@@ -0,0 +1,14 @@
+package com.taquillacentral.invitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeleccionarEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccionar_evento);
+    }
+}
