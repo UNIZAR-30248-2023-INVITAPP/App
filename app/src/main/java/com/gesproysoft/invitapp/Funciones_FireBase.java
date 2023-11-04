b745d61fd463c2bd2ad6733bfae90b36d811c9fa
815743 <815743@unizar.es>
2023-11-04 14:01:18 +0100

    He añadido la funciona validarInvitacionActualiza
    Esta función funciona igual que validarInvitacion, solo que cuando se detecta que está invitado, automáticamente se actualiza el campo "asistido" a true.

--- app/src/main/java/com/gesproysoft/invitapp/Funciones_FireBase.java
+++ app/src/main/java/com/gesproysoft/invitapp/Funciones_FireBase.java
@@ -3,6 +3,7 @@ package com.gesproysoft.invitapp;
 import android.util.Log;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -162,6 +163,73 @@ public class Funciones_FireBase {
             Log.d(tag, "Tarea no completada (a expìrado el timeout)");
         }
 
+        return false;
+    }
+
+
+    /**
+     * Comprueba que el invitado con ese DNI se encuentre invitado en el evento seleccionado
+     * Argumentos:
+     *  id_evento -> pasamos el identificador del evento donde comprobar si esta invitado
+     *  resultado -> guarda un List<Boolean> que contiene en su posicion 0, true o false
+     *              true    esta en la lista y actualizamos el campo "asistido" a true del invitado
+     *              false   no esta en la lista
+     * Devuelve:
+     *  true    la lectura de los datos a sido exitosa (aunque no haya contenido)
+     *  false   en caso de que salga algo mal(ha expirado timeout, no ha sido exitosa la lectura)
+     * */
+    private boolean validarInvitacionActualiza(String DNI, String id_evento, List<Boolean> resultado) {
+        String tag = "F_VALIDAR_INVIT";
+
+        // Access a Cloud Firestore instance from your Activity
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        CollectionReference collectRef = db.collection("Eventos").document(id_evento)
+                .collection("Invitados");
+
+        Task<QuerySnapshot> tarea;
+
+        resultado.clear();//Limpiamos el contenido de la lista
+
+        tarea = collectRef.whereEqualTo("dni", DNI).get();//Solicitamos la información del evento
+
+        //Comprobamos cada 0.20 segundos si ha terminado la tarea (maximo 3 segundos)
+        for(int i = 0; i < 15 && !tarea.isComplete(); i++){
+            //Log.d("F_INF_EVENTO", "Intento " + i + "-esimo");
+            try{
+                Thread.sleep(200);
+            } catch (InterruptedException e){
+                Log.d("EXCEPCION","Excepcion: " + e);
+            }
+        }
+
+        if(tarea.isComplete()){
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                Log.d("EXCEPCION",e.toString());
+            }
+            if(tarea.isSuccessful()){
+                QuerySnapshot query_docs = tarea.getResult();
+                if (query_docs.size() > 0) {
+                    DocumentSnapshot doc = query_docs.getDocuments().get(0);//Obtenemos el invitado que coincide
+                    DocumentReference docRef = db.collection("Eventos").document(id_evento)
+                            .collection("Invitados").document(doc.getId());
+                    docRef.update("asistido", true);//Actualizamos el campo "asistido" a true del invitado
+
+                    resultado.add(true);//Esta invitado
+                    Log.d(tag, "Hay invitado con ese DNI y se ha actualizado la asistencia");
+                } else {
+                    resultado.add(false);
+                    Log.d(tag, "No hay invitados con ese DNI");
+                }
+                return true;//Se ha establecido la comunicacion
+            } else {
+                Log.d(tag, "Lectura no exitosa");
+            }
+        } else {
+            Log.d(tag, "Tarea no completada (a expìrado el timeout)");
+        }
         return false;
     }
 }
